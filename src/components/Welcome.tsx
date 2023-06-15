5fd0eb605dbaacdd819a2cfa265250dfad426f8d
achraflab <labidiachraf6@gmail.com>
2023-06-15 16:15:13 +0200

    added a home screen and improved display for small screens

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/WelcomeScreen.css";
+
+function Welcome() {
+  return (
+    <div className="welcome-screen">
+      <h1 className="welcome-screen-title">
+        Welcome to the Patients Management System
+      </h1>
+      <div className="welcome-screen-options ">
+        <Link to="/search" className="welcome-screen-option search">
+          <span className="welcome-screen-option-icon">🔍</span>
+          <span className="welcome-screen-option-text">
+            Search and View Patients
+          </span>
+        </Link>
+        <Link to="/add" className="welcome-screen-option add">
+          <span className="welcome-screen-option-icon">➕</span>
+          <span className="welcome-screen-option-text">Add a New Patient</span>
+        </Link>
+        {/* TODO: EDIT Patients */}
+        <Link to="/edit" className="welcome-screen-option edit">
+          <span className="welcome-screen-option-icon">✏️</span>
+          <span className="welcome-screen-option-text">Edit a Patient</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
